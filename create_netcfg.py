ca9910df2ba07910c195af7817e6779c0afa49be
Rafael Amado <rgamado@gmail.com>
2020-09-29 10:45:35 -0400

    modified:   create_netcfg.py 	modified:   readNodeEdges.py

--- create_netcfg.py
+++ create_netcfg.py
@@ -25,41 +25,21 @@ fixPortSpeed = 100 #Mbps
 speeds = {}
 test={}
 
-# - READING EDGES AND NODES FROM OVS GENERATOR -
-# --------------- Read EDGES -------------------
-with open('windows-edge.json') as f:
-    edge_data = json.load(f)
+# # ------------------- Cleaning disconnected ------------------------ #
+# devices = base_ONOS.readDevices (ctrl)
+# for dev in devices:
+#     status = str(devices[dev][2])
+#     devNum = str(devices[dev][0])
+#     try:
+#         status.index('disconnected')
+#     except ValueError:
+#         print(status)
+#     else:
+#         print('Deleting device: ', devNum)
+#         base_ONOS.deleteDevice ( devNum, ctrl )
+# # ------------------------------------------------------------------ #
 
-for edge in edge_data:
-    linkId = str(edge_data[edge]['id'])
-    linkFrom = str(edge_data[edge]['from'])
-    linkTo = str(edge_data[edge]['to'])
-
-    try:
-        linkSpeed = str(edge_data[edge]['label'])
-    except KeyError:
-        # print('Link '+linkId+' has no link speed data')
-        linkSpeed = 'NotSpecified'
-        pass
-
-    print(linkId, linkFrom, linkTo, linkSpeed)
-
-
-# ----------- Read NODES -----------------
-with open('windows-node.json') as f:
-    node_data = json.load(f)
-
-for node in node_data:
-    nodeLabel = str(node_data[node]['label'])
-    nodeId = str(node_data[node]['id'])
-    print(nodeId, nodeLabel)
-
-
-
-
-
-
-# ---------------------- netcfg file POST -------------------------- #
+# ---------------------- Switch Name netcfg file POST -------------------------- #
 devices = base_ONOS.readDevices (ctrl)
 for dev in devices:
     devNum = str(devices[dev][0])
@@ -75,27 +55,92 @@ for dev in devices:
             print('Switch Name: '+portName)
             base_ONOS.config_netcfg_POST (ctrl, nameConfig)
         
-        else: 
-            # ------ Fixed Link Speeds - 100Mbps ------
-            # print('portNum: '+portNum)
-            # print('portName: '+portName)
-            # portConfig = {"ports": { str(devNum+"/"+portNum) : { "bandwidthCapacity": { "capacityMbps": 50 }}}}
-            # print('POSTTTTTTTTTTTTTTTTTTTTTTTTT')
-            portConfig = {"devices": {str(devNum): { "ports": { str(portNum): { "number": portNum, "speed": fixPortSpeed } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": fixPortSpeed } } }} # MUST ADAPT TO READ FILE WITH LINKS FROM OVS-MESH
-            base_ONOS.config_netcfg_POST (ctrl, portConfig)
-
-            # if portName in speeds:
-            #     try:
-            #         speeds[portName].append[fixPortSpeed]
-            #     except:
-            #         pass
-            # else:
-            #     speeds[portName] = fixPortSpeed
-
-## Link Speeds 
-# with open('speeds.json', 'w') as outfile:
-    # json.dump(test, outfile)
+        # else: 
+        #     # ------ Fixed Link Speeds - 100Mbps ------
+        #     # print('portNum: '+portNum)
+        #     # print('portName: '+portName)
+        #     # portConfig = {"ports": { str(devNum+"/"+portNum) : { "bandwidthCapacity": { "capacityMbps": 50 }}}}
+        #     # print('POSTTTTTTTTTTTTTTTTTTTTTTTTT')
+        #     portConfig = {"devices": {str(devNum): { "ports": { str(portNum): { "number": portNum, "speed": fixPortSpeed } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": fixPortSpeed } } }} # MUST ADAPT TO READ FILE WITH LINKS FROM OVS-MESH
+        #     base_ONOS.config_netcfg_POST (ctrl, portConfig)
 
 
-        # config2 = {"devices": {str(devNum): { "ports": { str(portNum): { "number": 1, "speed": 100 } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": 100 } } }}
-        # base_ONOS.config_netcfg_POST (ctrl, config2)
+# ---------------- Port Speeds Configuration | netcfg file POST ------------------
+
+edgesInfo = {}
+aux = {}
+
+
+# - READING EDGES AND NODES FROM OVS GENERATOR -
+# --------------- Read EDGES -------------------
+with open('windows-edge.json') as f:
+    edge_data = json.load(f)
+
+# ----------- Read NODES -----------------
+with open('windows-node.json') as f:
+    node_data = json.load(f)
+
+for edge in edge_data:
+    linkFROM = str(edge_data[edge]['from'])
+    linkID = str(edge_data[edge]['id'])
+    for node in node_data:
+        nodeID = str(node_data[node]['id'])
+        if nodeID == linkFROM:
+            nodeNAME = str(node_data[node]['label'])
+            edgesInfo[linkID] = {'from': nodeNAME}
+            linkTO = str(edge_data[edge]['to'])
+            for node in node_data:
+                nodeID = str(node_data[node]['id'])
+                nodeNAME = str(node_data[node]['label'])
+                if nodeID == linkTO:
+                    try:
+                        linkSPEED = str(edge_data[edge]['label'])
+                        edgesInfo[linkID].update({'to': nodeNAME, 'speed': linkSPEED})
+                    except KeyError:
+                        pass
+
+
+# print(edgesInfo)
+# for i in edgesInfo:
+#     try:
+#         edgePorts = 'c.'+edgesInfo[i]['from']+'-'+edgesInfo[i]['to']
+#         portSpeed = edgesInfo[i]['speed']
+#         print(edgePorts, portSpeed)
+#     except KeyError:
+#         pass 
+
+
+devices = base_ONOS.readDevices (ctrl)
+for dev in devices:
+    devNum = str(devices[dev][0])
+    ports = base_ONOS.readPorts(devNum, ctrl)
+    for port in ports:
+        portNum = str(ports[port][1])
+        portName = str(ports[port][3]["portName"])
+        if portNum != 'local' and len(portName) > 7 :
+            if int(portName[len(portName)-1]) == 1:  # only one interface in the same port ex: 'c.sw00-sw01.1'
+                for i in edgesInfo:
+                    try:
+                        # Check portNames vs Edges Info - one direction
+                        edgePortsFWD = 'c.'+edgesInfo[i]['from']+'-'+edgesInfo[i]['to']
+                        portSpeed = edgesInfo[i]['speed']
+                        # print('CheckPoint FWD')
+                        # print(edgePortsFWD, portName[:-2])
+                        if str(edgePortsFWD) == str(portName[:-2]):
+                            portConfig = {"devices": {str(devNum): { "ports": { str(portNum): { "number": portNum, "speed": portSpeed } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": portSpeed } } }} # MUST ADAPT TO READ FILE WITH LINKS FROM OVS-MESH
+                            print(portConfig)
+                            base_ONOS.config_netcfg_POST (ctrl, portConfig)
+
+                        # Check portNames vs Edges Info - other direction
+                        edgePortsRET = 'c.'+edgesInfo[i]['to']+'-'+edgesInfo[i]['from']
+                        portSpeed = edgesInfo[i]['speed']
+                        # print('CheckPoint RET')
+                        # print(edgePortsRET, portName[:-2])
+                        if str(edgePortsRET) == str(portName[:-2]):
+                            portConfig = {"devices": {str(devNum): { "ports": { str(portNum): { "number": portNum, "speed": portSpeed } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": portSpeed } } }} # MUST ADAPT TO READ FILE WITH LINKS FROM OVS-MESH
+                            print(portConfig)
+                            base_ONOS.config_netcfg_POST (ctrl, portConfig)
+                    except KeyError:
+                        pass               
+            else:
+                print('More than one interface in this port')
--- readNodeEdges.py
+++ readNodeEdges.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # imports
 import json, requests, sys, time, os
 import os.path
@@ -12,19 +14,10 @@ container = client.containers.get('onos')
 ctrlIP = container.attrs['NetworkSettings']['IPAddress']
 
 ctrl = 'http://'+ctrlIP+':8181/onos/v1/'
-
-
-# devices = base_ONOS.readDevices (ctrl)
-# for dev in devices:
-#     devNum = str(devices[dev][0])
-#     ports = base_ONOS.readPorts(devNum, ctrl)
-#     for port in ports:
-#         portNum = str(ports[port][1])
-#         portName = str(ports[port][3]["portName"])
-#         print(portName, portNum)
         
 edgesInfo = {}
 aux = {}
+
 
 # - READING EDGES AND NODES FROM OVS GENERATOR -
 # --------------- Read EDGES -------------------
@@ -38,15 +31,12 @@ with open('windows-node.json') as f:
 for edge in edge_data:
     linkFROM = str(edge_data[edge]['from'])
     linkID = str(edge_data[edge]['id'])
-    # linkSPEED = str(edge_data[edge]['label'])
     for node in node_data:
         nodeID = str(node_data[node]['id'])
         if nodeID == linkFROM:
             nodeNAME = str(node_data[node]['label'])
             edgesInfo[linkID] = {'from': nodeNAME}
-            # print('from: '+str(node_data[node]['label']))
             linkTO = str(edge_data[edge]['to'])
-            # print(linkTO)
             for node in node_data:
                 nodeID = str(node_data[node]['id'])
                 nodeNAME = str(node_data[node]['label'])
@@ -54,14 +44,51 @@ for edge in edge_data:
                     try:
                         linkSPEED = str(edge_data[edge]['label'])
                         edgesInfo[linkID].update({'to': nodeNAME, 'speed': linkSPEED})
-
                     except KeyError:
                         pass
-                    # try:
-                    #     nodeLabel.index('sw')
-                    # except ValueError:
-                    #     pass
-                    # else:
-                    #     print('speed: '+str(edge_data[edge]['label']))
 
-print(edgesInfo)
+
+# print(edgesInfo)
+# for i in edgesInfo:
+#     try:
+#         edgePorts = 'c.'+edgesInfo[i]['from']+'-'+edgesInfo[i]['to']
+#         portSpeed = edgesInfo[i]['speed']
+#         print(edgePorts, portSpeed)
+#     except KeyError:
+#         pass 
+
+
+devices = base_ONOS.readDevices (ctrl)
+for dev in devices:
+    devNum = str(devices[dev][0])
+    ports = base_ONOS.readPorts(devNum, ctrl)
+    for port in ports:
+        portNum = str(ports[port][1])
+        portName = str(ports[port][3]["portName"])
+        if portNum != 'local' and len(portName) > 7 :
+            if int(portName[len(portName)-1]) == 1:  # only one interface in the same port ex: 'c.sw00-sw01.1'
+                for i in edgesInfo:
+                    try:
+                        # Check portNames vs Edges Info - one direction
+                        edgePortsFWD = 'c.'+edgesInfo[i]['from']+'-'+edgesInfo[i]['to']
+                        portSpeed = edgesInfo[i]['speed']
+                        # print('CheckPoint FWD')
+                        # print(edgePortsFWD, portName[:-2])
+                        if str(edgePortsFWD) == str(portName[:-2]):
+                            portConfig = {"devices": {str(devNum): { "ports": { str(portNum): { "number": portNum, "speed": portSpeed } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": portSpeed } } }} # MUST ADAPT TO READ FILE WITH LINKS FROM OVS-MESH
+                            print(portConfig)
+                            # base_ONOS.config_netcfg_POST (ctrl, portConfig)
+
+                        # Check portNames vs Edges Info - other direction
+                        edgePortsRET = 'c.'+edgesInfo[i]['to']+'-'+edgesInfo[i]['from']
+                        portSpeed = edgesInfo[i]['speed']
+                        # print('CheckPoint RET')
+                        # print(edgePortsRET, portName[:-2])
+                        if str(edgePortsRET) == str(portName[:-2]):
+                            portConfig = {"devices": {str(devNum): { "ports": { str(portNum): { "number": portNum, "speed": portSpeed } } } },"ports": {str(devNum)+"/"+str(portNum): {"bandwidthCapacity": { "capacityMbps": portSpeed } } }} # MUST ADAPT TO READ FILE WITH LINKS FROM OVS-MESH
+                            print(portConfig)
+                            # base_ONOS.config_netcfg_POST (ctrl, portConfig)
+                    except KeyError:
+                        pass               
+            else:
+                print('More than one interface in this port')
